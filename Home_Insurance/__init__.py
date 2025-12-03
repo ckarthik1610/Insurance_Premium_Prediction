a3e3eb2d06dd14ce8aa606a5b60ff91265e82484
ilakiya190404 <ilakz1904@gmail.com>
2025-12-02 19:51:45 -0800

    __init__.py

--- Home_Insurance/__init__.py
+++ Home_Insurance/__init__.py
@@ -1,21 +1,4 @@
-"""
-Home Insurance sub-package.
+from .risk_factor import data
+from .premium_calculator import Predict
 
-This sub-package contains functionality to:
-- Extract and process property and environmental features.
-- Compute risk scores.
-- Train and interpret models to estimate home insurance premiums.
-"""
-
-from .risk_factor import (
-    calculate_property_risk,
-    environmental_risk_score,
-    summarize_home_profile,
-)
-
-from .premium_calculator import (
-    HomePremiumEstimator,
-    train_home_model,
-    predict_home_premium,
-    model_interpretation,
-)
+_all_ = ["data", "Predict"]
