1269e8197ab3d6218b9646a0ed3bbf93b73788df
spandandey12 <spandandey021@gmail.com>
2025-12-09 12:09:05 -0800

    Added some changes

--- tests/.ipynb_checkpoints/testHelper-checkpoint.py
+++ tests/.ipynb_checkpoints/testHelper-checkpoint.py
@@ -1,27 +1,36 @@
 import unittest
 import pandas as pd
 
+
 class TestHelper(unittest.TestCase):
+
     @classmethod
     def setUpClass(cls):
-        print("Started TestHelper.")
+        print("\nStarted TestHelper.")
+
     @classmethod
     def tearDownClass(cls):
-        print("Finished TestHelper.")
+        print("Finished TestHelper.\n")
+
     def setUp(self):
-        self.df = pd.DataFrame({"a":[1,2,3]})
+        self.df = pd.DataFrame({"a": [1, 2, 3]})
+
     def tearDown(self):
         pass
+
     def test_dataframe(self):
-        self.assertEqual(len(self.df),3)
-        self.assertIn("a",self.df.columns)
+        self.assertEqual(len(self.df), 3)
+        self.assertIn("a", self.df.columns)
         self.assertEqual(self.df["a"].sum(), 6)
         self.assertIsInstance(self.df.iloc[0]["a"], int)
+
     def test_math(self):
-        x,y=4,5
+        x, y = 4, 5
         self.assertEqual(x + y, 9)
         self.assertNotEqual(x * y, 10)
         self.assertGreater(y, x)
         self.assertIsInstance(x, int)
+
+
 if __name__ == "__main__":
-    unittest.main()
+    unittest.main(argv=[''], verbosity=2, exit=False)
